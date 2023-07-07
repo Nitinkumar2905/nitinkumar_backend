e78eafeb4861c329e6481c36fec4d2b1d4e680c7
Nitin kumar <nimble2905@gmail.com>
2023-07-07 22:44:48 +0530

    some changes

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const connectToMongo = require("./db");
 const express = require("express");
-const cors = require("cors");
+var cors = require("cors");
 
 connectToMongo();
 const app = express();
@@ -13,5 +13,5 @@ app.use(cors());
 app.use("/api/auth", require("./routes/auth"));
 
 app.listen(port, () => {
-  console.log(` nitinkumar.in backend running at ${port}`);
+  console.log(` nitinkumar.in backend running at http://localhost:${port}`);
 });
