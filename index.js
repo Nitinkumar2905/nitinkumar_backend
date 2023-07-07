54235178a6dd1ee411bc1fe8d5c76c9cf4da1e1b
Nitin kumar <nimble2905@gmail.com>
2023-07-07 21:53:44 +0530

    ready to deploy to vercel.app

--- index.js
+++ index.js
@@ -10,7 +10,13 @@ const app = express();
 const PORT = process.env.PORT || 8000;
 
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: ["https://deploy-mern-stack.vercel.app"],
+    methods: ["POST", "DELETE"],
+    credentials: true,
+  })
+);
 
 // available routes
 app.use("/api/auth", require("./routes/auth"));
